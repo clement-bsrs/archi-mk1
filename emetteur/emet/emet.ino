cf1b4312d9256a62b41eb5581e44eb2111e398a8
clement-bsrs <clementbessieres.pro@gmail.com>
2025-04-30 09:47:45 +0200

    archi avancé

--- emetteur/emet/emet.ino
+++ emetteur/emet/emet.ino
@@ -3,6 +3,17 @@
 */
 
 #include <VirtualWire.h>
+#include <LiquidCrystal.h>
+#include <BME280I2C.h>
+#include <Wire.h>
+
+// écran cristaux liquide
+const int rs = 12, en = 11, d4 = 37, d5 = 35, d6 = 33, d7 = 31;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// capteur pression, température, humidité
+BME280I2C bme;  // Default : forced mode, standby time = 1000 ms
+                // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
 
 const char ID_ARDUINO[] = "01";  // ID_ARDUINO doit être une chaîne de caractères constante
 int ID_TRAME = 0;                 // ID_TRAME doit être un entier, pas un char
@@ -16,46 +27,144 @@ long t = 0;
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("START : ");
+  Serial.begin(9600); // initialisation du port serie
+  Serial.println("ok");
+
+  lcd.begin(16, 2); // initialisation de l'écran
+  lcd.setCursor(0,0);
+  lcd.print("initialisation");
+  delay(1000);
+  lcd.clear();
+
+  // while(!Serial) {} // Wait
+  Wire.begin();
+  while(!bme.begin())
+  {
+    lcd.print("capteur-NF");
+    delay(1000);
+  }
+  switch(bme.chipModel())
+  {
+    case BME280::ChipModel_BME280:
+      lcd.print("BME280-OK");
+      delay(1000);
+      lcd.clear();
+      break;
+    case BME280::ChipModel_BMP280:
+      lcd.print("BME280-OK-noH");
+      delay(1000);
+      lcd.clear();
+      break;
+    default:
+      lcd.print("Ukn-sens");
+      delay(1000);
+      lcd.clear();
+  }
+  delay(1000);
+  // - - - Liaison radio - - -
   vw_set_tx_pin(RF_TX_PIN); // initialisation de la broche d'envoi
   vw_set_rx_pin(RF_RX_PIN);  // initialisation de la broche de réception
-  vw_setup(2000); // choix de la vitesse de transmission
-  vw_rx_start(); // démarrage du récepteur
-  delay(2000); // attend 2 seconde avant d'envoyer la première trame
+  vw_setup(2000); // choix de la vitesse de transmission rf
+  vw_rx_start(); // démarrage du récepteur rf
+  lcd.print("rf-ok");
+  delay(1000);
+  lcd.clear();
 }
+/*
+
+void send(char msg[50]){
+  // Envoi du message
+  int res = vw_send((uint8_t *)msg, 1 + strlen(msg)); // envoi du message
+  if (res){
+    Serial.print(msg);
+    Serial.println("Message envoyé avec succès");
+  }
+  else {
+    Serial.println("Erreur lors de l'envoi");
+  }
+}
+
+*/
 
 void loop()
 {
-  if(millis() == current + 3000){
-  // char msg[50] = "";
 
-  // // Créer la trame avec l'ID_ARDUINO et l'ID_TRAME
-  // sprintf(msg, "XX:%s:%02d:00:00:00:00:00", ID_ARDUINO, ID_TRAME);
-  
-  // Serial.print("Envoi d'un message : ");
-  // Serial.println(msg);
 
-  // // Envoi du message
-  // int res = vw_send((uint8_t *)msg, 1 + strlen(msg)); // envoi du message
-  // if (res)
-  //   Serial.println("Message envoyé avec succès");
-  // else
-  //   Serial.println("Erreur lors de l'envoi");
-  // Serial.println("bonjour");
-  current = millis();
+      // relève les constante de la pièce -  -
+
+  // lecture et enregistrement des valeurs ...
+  float temp(NAN), hum(NAN), pres(NAN);
+  bme.read(pres, temp, hum, bme.TempUnit_Celsius, bme.PresUnit_hPa);
+
+  // Affichage sur l'écran lcd
+  lcd.clear();
+  lcd.setCursor(2,0);
+  lcd.print("oC");
+  lcd.setCursor(4,0);
+  lcd.print(" hPa");
+  lcd.setCursor(9,0);
+  lcd.print("%RH");
+
+  lcd.setCursor(2,1);
+  lcd.print((int)temp);
+  lcd.setCursor(5,1);
+  lcd.print((int)pres);
+  lcd.setCursor(9,1);
+  lcd.print((int)hum);
+
+  // Envoie des trames toutes les 15 secondes
+  if(millis() >= current + 15000){ // Delai non-bloquant (15 secondes)
+
+    // Incrémenter l'ID_TRAME et gérer le dépassement de 100
+    ID_TRAME++;
+    if (ID_TRAME == 100) {
+      ID_TRAME = 0; // Réinitialisation à 0 lorsqu'on atteint 100
+    }
+    // préparation de la trame à envoyer
+    char msg[50] = "";
+
+    // Créer la trame avec l'ID_ARDUINO et l'ID_TRAME
+    // 0 ,1        -  XX        ->  identifiant du protocole
+    // 2 ,3        -  ID_AR     ->  id de l'arduino
+    // 4 ,5        -  ID_TR     ->  id de la trame
+    // 6 ,7        -  Temp      ->  température
+    // 8 ,9        -  Hum       ->  Humidité
+    // 10,11,12    -  Pres      ->  Pression atmosphérique
+
+
+    sprintf(msg, "XX:%s:%02d:%d:%d:%d:%df:ff", ID_ARDUINO, ID_TRAME, (int)temp, (int)hum, (int)pres/10, (int)pres%10);
+    // Serial.println((int)pres/10);
+    // Serial.println((int)pres%10);
+    Serial.println("envoie ... ");
+    // send(msg);
+
+    // Envoi du message
+    int res = vw_send((uint8_t *)msg, 1 + strlen(msg)); // envoi du message
+    if (res){
+      Serial.println(msg);
+      Serial.println("Message envoyé avec succès");
+      lcd.setCursor(15,1);
+      lcd.print("*");
+      delay(100);
+      lcd.setCursor(15,1);
+      lcd.print(" ");
+      
+    }
+    else {
+      Serial.println("Erreur lors de l'envoi");
+    }
+
+    current = millis();
   }
+  
 
   // TEST RFID -------------------------------------
   
+  // A COMPLETER
+
   // -----------------------------------------------
 
 
-  // Incrémenter l'ID_TRAME et gérer le dépassement de 100
-  ID_TRAME++;
-  if (ID_TRAME == 100) {
-    ID_TRAME = 0; // Réinitialisation à 0 lorsqu'on atteint 100
-  }
 
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
@@ -73,7 +182,7 @@ void loop()
     Serial.print((char) buf[6]); // X id arduino 1
     Serial.println((char) buf[7]); // X id arduino 2
 
-    // Si le message vient du serveur
+    // le message vient du serveur ?
     if ((char) buf[6] == '0' && (char) buf[7] == '0')
     {
       Serial.print("Message du serveur | ");
